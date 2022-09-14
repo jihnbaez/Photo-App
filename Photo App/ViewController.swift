b3b3b9b16987dce006e4da2235ad4165da78578e
John <baezjohn312@gmail.com>
2022-09-14 13:12:17 -0400

    Initial Commit

---
+++ Photo App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo App
+//
+//  Created by 8h on 9/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
