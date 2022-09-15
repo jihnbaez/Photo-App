c93d81b225ad4aad371cce5c2195d3b6d42e069b
John <baezjohn312@gmail.com>
2022-09-15 13:35:25 -0400

    ok

--- Photo App/ViewController.swift
+++ Photo App/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var greetLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func greetButton(_ sender: Any) {
+        greetLabel.text = "Hello" +  textField.text!
+    }
+    
 }
 
